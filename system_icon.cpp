547f2d62718992cf2b50efb881840b013a090ef8
Josephus Paye II <j.paye96@gmail.com>
2018-09-22 21:28:42 +1000

    Cleanup system_icon.cpp and system_icon.hpp

--- system_icon.cpp
+++ system_icon.cpp
@@ -1,13 +1,11 @@
 #include "system_icon.hpp"
 
 template <>
-void SystemIconAsyncWorker<ExtensionTag>::Execute()
-{
-  // Do nothing.
+void SystemIconAsyncWorker<ExtensionTag>::Execute() {
+    // Do nothing: see OS-specific files for actual implementation
 }
 
 template <>
-void SystemIconAsyncWorker<PathTag>::Execute()
-{
-  // Do nothing.
+void SystemIconAsyncWorker<PathTag>::Execute() {
+    // Do nothing: see OS-specific files for actual implementation
 }
--- system_icon.hpp
+++ system_icon.hpp
@@ -1,58 +1,43 @@
 #include <nan.h>
 #include <string>
 
-enum class IconSize
-{
-  ExtraSmall,
-  Small,
-  Medium,
-  Large,
-  ExtraLarge
+enum class IconSize {
+    ExtraSmall,
+    Small,
+    Medium,
+    Large,
+    ExtraLarge
 };
 
-struct ExtensionTag
-{
-};
-
-struct PathTag
-{
-};
+struct ExtensionTag {};
+struct PathTag {};
 
 template <class Tag>
-class SystemIconAsyncWorker : public Nan::AsyncWorker
-{
-public:
-  SystemIconAsyncWorker(const char* name, IconSize size,
-                        Nan::Callback* callback)
-    : Nan::AsyncWorker{callback}, name{name}, size{size}
-  {
-  }
+class SystemIconAsyncWorker : public Nan::AsyncWorker {
+    public:
+      SystemIconAsyncWorker(const char* name, IconSize size, Nan::Callback* callback) : Nan::AsyncWorker{callback}, name{ name }, size{ size } {}
+      void Execute() override;
 
-  void Execute() override;
+    protected:
+        void HandleOKCallback() override {
+            if (!this->result.empty()) {
+                v8::Local<v8::Value> argv[] = {
+                    Nan::Null(),
+                    Nan::CopyBuffer(
+                        static_cast<char*>(static_cast<void*>(this->result.data())),
+                        this->result.size()
+                    ).ToLocalChecked(),
+                };
 
-protected:
-  void HandleOKCallback() override
-  {
-    if (!this->result.empty())
-    {
-      v8::Local<v8::Value> argv[] = {
-        Nan::Null(),
-        Nan::CopyBuffer(
-          static_cast<char*>(static_cast<void*>(this->result.data())),
-          this->result.size())
-          .ToLocalChecked(),
-      };
-      callback->Call(2, argv);
-    }
-    else
-    {
-      v8::Local<v8::Value> argv[] = {Nan::Error("Failed to load icon")};
-      callback->Call(1, argv);
-    }
-  }
+                callback->Call(2, argv);
+            } else {
+                v8::Local<v8::Value> argv[] = {Nan::Error("Failed to load icon")};
+                callback->Call(1, argv);
+            }
+        }
 
-private:
-  std::string name;
-  IconSize size;
-  std::vector<unsigned char> result;
+    private:
+        std::string name;
+        IconSize size;
+        std::vector<unsigned char> result;
 };
